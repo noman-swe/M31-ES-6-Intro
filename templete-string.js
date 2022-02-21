72169423399d1f95c0c18dbbca9b4d4a21748572
noman-swe <noman.swe130@gmail.com>
2022-02-22 01:06:55 +0600

    31-5 Template string, multiple line string, dynamic string

---
+++ templete-string.js
@@ -0,0 +1,22 @@
+const riya = 'asif akbor';
+const meye = "Meye tmi ki dukkho chino";
+const kobita = `kobita tmi ki shopno`;
+const multiLine = 'This is my 1st line \n' + 'This is my 2nd line \n' + 'This is my 3rd line \n' + 'This is my 4th line \n';
+// console.log(multiLine);
+const newMultiLine = `This is multiline 
+this is 2nd
+this is 3rd
+4th`;
+console.log(newMultiLine);
+
+const friendCount = 5; 
+const old = '<h3 class="friend-name">Friend 3</h3>';
+const old2 = '<h3 class="friend-name">Friend' + friendCount + '</h3>';
+const new1 = `<h3 class="friend-name">Friend ${friendCount}</h3>`;
+console.log(new1);
+
+const first = 'noman';
+const last = 'Shibly';
+const fullOld = first + ' ' + last;
+const fullNew = `${first} ${last}`;  
+console.log(fullNew);
